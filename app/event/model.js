e2de3088de381df557df5e476d89366a01108568
alekspetrov <mr.alex.petrov@gmail.com>
2015-06-11 12:27:11 +1000

    Date => dateFormat

--- app/event/model.js
+++ app/event/model.js
@@ -5,7 +5,7 @@ export default DS.Model.extend({
     title: 			attr('string'),
     review: 		attr('string'),
     cover: 			attr('string'),
-    date: 			attr('string'),
+    date: 			attr('string', 	{ defaultValue: '0001-01-01T00:00:00.000000+10:00' }),
     age: 			attr('number', 	{ defaultValue: '16' }), 
     promoter: 		attr('string'),
     created_at: 	attr('string', 	{ defaultValue: '0001-01-01T00:00:00.000000+10:00' }), 
